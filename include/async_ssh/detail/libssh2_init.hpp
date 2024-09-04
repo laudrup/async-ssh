d9447a1fc122164506a7c3e938ab8022e1d7e40a
Kasper Laudrup <laudrup@stacktrace.dk>
2024-09-04 21:55:11 +0200

    Fix multiple definition of libssh2_init
    Just implement libssh2_init directly in its class to avoid multiple
    definition errors instead of trying to be clever.

--- include/async_ssh/detail/libssh2_init.hpp
+++ include/async_ssh/detail/libssh2_init.hpp
@@ -1,22 +1,26 @@
 #ifndef ASYNC_SSH_DETAIL_LIBSSH2_INIT_HPP
 #define ASYNC_SSH_DETAIL_LIBSSH2_INIT_HPP
 
+#include <libssh2.h>
+
 namespace async_ssh::detail {
 
 class libssh2_init {
 public:
-  libssh2_init() noexcept;
-  ~libssh2_init();
+  libssh2_init() noexcept {
+    ::libssh2_init(0);
+  }
+  ~libssh2_init() {
+    ::libssh2_exit();
+  }
   libssh2_init(const libssh2_init&) = delete;
   libssh2_init& operator=(const libssh2_init&) = delete;
   libssh2_init(libssh2_init&&) = delete;
   libssh2_init operator=(libssh2_init&&) = delete;
 };
 
-static inline const libssh2_init libssh2_init_instance;
+static const libssh2_init libssh2_init_instance;
 
 }  // namespace async_ssh::detail
-
-#include <async_ssh/detail/impl/libssh2_init.ipp>
 
 #endif // ASYNC_SSH_DETAIL_LIBSSH2_INIT_HPP
