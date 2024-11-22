bf4052686fa0a4e3b990312c3f975f66755632b3
Kasper Laudrup <laudrup@stacktrace.dk>
2024-11-22 19:44:06 +0100

    Fix path conversion on Windows
    On Windows .c_str() for paths will return a wchar_t so convert the
    path to a standard string to do convert the path first.
    
    This most likely in no way guarantees that this will actually create a
    valid UTF-8 string but for now it will at least compile and handle
    ASCII paths.

--- include/async_ssh/session.hpp
+++ include/async_ssh/session.hpp
@@ -403,7 +403,8 @@ public:
   std::tuple<channel, async_ssh::remote_directory_entry> scp_recv(const std::filesystem::path& path, std::error_code& ec) {
     api::libssh2_session_set_blocking(handle(), 1);
     remote_directory_entry entry{};
-    LIBSSH2_CHANNEL* chan = api::libssh2_scp_recv2(handle(), path.c_str(), &entry.stat_);
+    auto path_str = path.string();
+    LIBSSH2_CHANNEL* chan = api::libssh2_scp_recv2(handle(), path_str.c_str(), &entry.stat_);
     if (chan == nullptr) {
       ec = std::error_code(api::libssh2_session_last_errno(handle()), libssh2_error_category());
     } else {
